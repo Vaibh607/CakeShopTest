a6e8f3ecf0b888963fcfdfd110ba97f0441dc519
Vaibhav <admin@promilo.com>
2025-10-29 18:14:28 +0530

    restarting deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Set the base path for GitHub Pages deployment (repo name)
+  base: '/CakeShopTest/',
 })
